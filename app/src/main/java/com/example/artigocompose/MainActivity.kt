09a2fcfb36d5007976f53c605405729072f9a404
Silpharny <143123706+Silpharny@users.noreply.github.com>
2023-11-03 10:54:47 -0300

    Initial commit

---
+++ app/src/main/java/com/example/artigocompose/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.artigocompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.artigocompose.ui.theme.ArtigoComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ArtigoComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    BasicLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicLayout(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+
+    Column {
+
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+
+        Text(
+            text = stringResource(R.string.title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.subtitle),
+            modifier = Modifier
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp
+                ),
+            textAlign = TextAlign.Justify
+        )
+
+        Text(
+            text = stringResource(R.string.description),
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    ArtigoComposeTheme {
+    BasicLayout()
+    }
+}
